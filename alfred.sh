f5fa2f01b7b044a1308b4d6fbf26880a12fc344e
will Farrell <will.farrell@gmail.com>
2013-10-02 12:16:26 -0600

    added version option

--- alfred.sh
+++ alfred.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+VERSION="0.1";
 
 function help() {
-	echo "alfred [options] [-f] <file> [-q] <query>"
-	echo ""
-	echo "  -f  --file   Filename of php file (ex 'script.php')"
-	echo "  -h  --help   Show help options (what you're viewing now)"
-	echo "  -q  --query  Value to replace {query} with"
+    echo "alfred [options] [-f] <file> [-q] <query>";
+    echo "";
+    echo "  -f  --file     Filename of php file (ex 'script.php')";
+    echo "  -h  --help     Show help options (what you're viewing now)";
+    echo "  -q  --query    Value to replace {query} with";
+    echo "  -v  --version  Return version of script";
 }
 
-FILE='';
-QUERY='';
+FILE='';   # Filename from --file
+QUERY='';  # String from --query
+OUTPUT=''; # 
+VERSION_STR="Version: $VERSION";
 
 while true ; do
     case "$1" in
@@ -37,19 +41,30 @@ while true ; do
             QUERY=$2
             shift 2
         ;;
+        -v )
+            OUTPUT=$VERSION_STR
+            break
+        ;;
+        --version )
+            OUTPUT=$VERSION_STR
+            break
+        ;;
         *)
             break
         ;;
     esac 
 done;
 
-if [ $FILE ]; then
-	EXT="${FILE##*.}";
-	if [ $EXT ]; then
-		echo "Common batman, you need a file extension."
-	else
-		cat $FILE | sed -e "s/{query}/$QUERY/" > .tmp && $EXT .tmp && rm .tmp;
-	fi
+if [[ $FILE ]]; then
+    EXT="${FILE##*.}";
+    if [[ $EXT ]]; then
+        echo "Common batman, you need a file extension."
+    else
+        cat $FILE | sed -e "s/{query}/$QUERY/" > .tmp && $EXT .tmp && rm .tmp;
+    fi
+elif [[ $OUTPUT ]]; then
+    echo $OUTPUT
 else
-	help
+    # Called from -h, --help, or when no options are passed
+    help
 fi
